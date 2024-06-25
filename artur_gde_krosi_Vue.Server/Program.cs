b7044dfd25241b0d4d00891b70389a3dce074f2b
xXxandry2xXx <112953841+xXxandry2xXx@users.noreply.github.com>
2024-06-25 23:08:20 +0500

    Осуществил поддержку докера

--- artur_gde_krosi_Vue.Server/Program.cs
+++ artur_gde_krosi_Vue.Server/Program.cs
@@ -19,114 +19,118 @@ using artur_gde_krosi_Vue.Server.Services.EmailService;
 using artur_gde_krosi_Vue.Server.Services.ControlerService;
 using artur_gde_krosi_Vue.Server.Services.ControlerService.CharacteristicProductServices;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddControllers().AddJsonOptions(x =>
-    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
-
-
-string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<ApplicationIdentityContext>(options => options.UseSqlServer(connection));
-
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+internal class Program
 {
-    options.SignIn.RequireConfirmedAccount = true;
-    options.User.RequireUniqueEmail = true;
-    options.User.RequireUniqueEmail = true;
-})
-.AddDefaultTokenProviders()
-.AddEntityFrameworkStores<ApplicationIdentityContext>();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
-{
-    ValidateActor = true,
-    ValidateIssuer = true,
-    ValidateAudience = true,
-    ValidateIssuerSigningKey = true,
-    ValidIssuer = builder.Configuration.GetSection("JWT:Issuer").Value,
-    ValidAudience = builder.Configuration.GetSection("JWT:Audience").Value,
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT:key").Value))
-});
-
-builder.Services.AddMemoryCache();
-
-builder.Services.AddScoped<ProductService>();
-builder.Services.AddTransient<EmailBodyService>();
-builder.Services.AddTransient<ShoppingCartService>();
-builder.Services.AddTransient<MailingMailSerivce>();
-builder.Services.AddTransient<FilterService>();
-builder.Services.AddTransient<CharacteristicProductsService>();
-builder.Services.AddTransient<CharacteristicProductValueService>();
-
-builder.Services.AddTransient<ParserService>();
-builder.Services.AddScoped<IAccountService, AccountService>();
-builder.Services.AddScoped<IAccountValidationService, AccountValidationService>();
-builder.Services.AddScoped<IAccountMailingTokenService, AccountMailingTokenService>();
-builder.Services.AddScoped<IAccountSettingsService, AccountSettingsService>();
-
-builder.Services.AddScoped<IGroupParserService, GroupParserService>();
-builder.Services.AddScoped<IAllProductParserService, ProductParserService>();
-builder.Services.AddScoped<IStokProductParserService, ProductParserService>();
-builder.Services.AddScoped<IPostImegesS3Service, PostImegesS3Service>();
-
-builder.Services.AddTransient<IEmailService, EmailService>();
-
-IServiceCollection services = builder.Services;
-
-services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("https://localhost:5173")
-                          .AllowAnyHeader()
-                          .AllowAnyMethod());
-});
-
-// Add Quartz services
-services.AddHostedService<QuartzHostedService>();
-services.AddSingleton<IJobFactory, SingletonJobFactory>();
-services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
-
-services.AddSingleton<ProductAndGroupJob>();
-services.AddSingleton(new JobSchedule(
-    jobType: typeof(ProductAndGroupJob),
-    cronExpression: "0  20 0  ? * *"));
-
-services.AddSingleton<UpdateStockJob>();
-services.AddSingleton(new JobSchedule(
-    jobType: typeof(UpdateStockJob),
-    cronExpression: "0 0/15  * ? * *"));
-
-services.AddAWSService<IAmazonS3>();
-
-var app = builder.Build();
-
-app.UseDefaultFiles();
-app.UseStaticFiles();
-app.MapControllers();
-app.UseCors("AllowSpecificOrigin");
-app.UseAuthorization();
-app.UseMiddleware<ExceptionHandingMiddleware>();
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddJsonOptions(x =>
+            x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
+        string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+        builder.Services.AddDbContext<ApplicationIdentityContext>(options => options.UseSqlServer(connection));
+
+
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = true;
+            options.User.RequireUniqueEmail = true;
+            options.User.RequireUniqueEmail = true;
+        })
+        .AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<ApplicationIdentityContext>();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateActor = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetSection("JWT:Issuer").Value,
+            ValidAudience = builder.Configuration.GetSection("JWT:Audience").Value,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT:key").Value))
+        });
+
+        builder.Services.AddMemoryCache();
+
+        builder.Services.AddScoped<ProductService>();
+        builder.Services.AddTransient<EmailBodyService>();
+        builder.Services.AddTransient<ShoppingCartService>();
+        builder.Services.AddTransient<MailingMailSerivce>();
+        builder.Services.AddTransient<FilterService>();
+        builder.Services.AddTransient<CharacteristicProductsService>();
+        builder.Services.AddTransient<CharacteristicProductValueService>();
+
+        builder.Services.AddTransient<ParserService>();
+        builder.Services.AddScoped<IAccountService, AccountService>();
+        builder.Services.AddScoped<IAccountValidationService, AccountValidationService>();
+        builder.Services.AddScoped<IAccountMailingTokenService, AccountMailingTokenService>();
+        builder.Services.AddScoped<IAccountSettingsService, AccountSettingsService>();
+
+        builder.Services.AddScoped<IGroupParserService, GroupParserService>();
+        builder.Services.AddScoped<IAllProductParserService, ProductParserService>();
+        builder.Services.AddScoped<IStokProductParserService, ProductParserService>();
+        builder.Services.AddScoped<IPostImegesS3Service, PostImegesS3Service>();
+
+        builder.Services.AddTransient<IEmailService, EmailService>();
+
+        IServiceCollection services = builder.Services;
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowSpecificOrigin",
+                builder => builder.WithOrigins("https://localhost:5173")
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod());
+        });
+
+        // Add Quartz services
+        services.AddHostedService<QuartzHostedService>();
+        services.AddSingleton<IJobFactory, SingletonJobFactory>();
+        services.AddSingleton<ISchedulerFactory, StdSchedulerFactory>();
+
+        services.AddSingleton<ProductAndGroupJob>();
+        services.AddSingleton(new JobSchedule(
+            jobType: typeof(ProductAndGroupJob),
+            cronExpression: "0  20 0  ? * *"));
+
+        services.AddSingleton<UpdateStockJob>();
+        services.AddSingleton(new JobSchedule(
+            jobType: typeof(UpdateStockJob),
+            cronExpression: "0 0/15  * ? * *"));
+
+        services.AddAWSService<IAmazonS3>();
+
+        var app = builder.Build();
+        app.UseAuthorization();
+        app.UseMiddleware<ExceptionHandingMiddleware>();
+        app.MapControllers();
+        //app.UseDefaultFiles();
+
+        //app.UseStaticFiles();
+        app.UseCors("AllowSpecificOrigin");
+        //app.MapFallbackToFile("/index.html");
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.Run();
+    }
 }
-
-
-app.MapFallbackToFile("/index.html");
-app.Run();
