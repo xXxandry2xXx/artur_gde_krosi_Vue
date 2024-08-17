0cfbfd948c83e995f56fc5225e174d832bca51c5
Кирилл Стрижак <hardcore_chel@mail.ru>
2024-08-17 22:05:43 +0300

    мелочь

--- artur_gde_krosi_vue.client/vite.config.ts
+++ artur_gde_krosi_vue.client/vite.config.ts
@@ -1,19 +1,11 @@
-import { fileURLToPath, URL } from 'node:url';
-
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-
-
-export default defineConfig({
-    plugins: [vue()],
-    server: {
-        host: '0.0.0.0', // прослушивать все интерфейсы
-        port: 4173,
-    },
+    plugins: [
+        vue()
+    ],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    }
+    },
 })
